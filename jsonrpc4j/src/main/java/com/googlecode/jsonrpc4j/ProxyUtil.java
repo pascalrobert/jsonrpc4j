a4fa9e3da9d3bb68fe9a164b67dc11c9b485b1de
brian.dilley <brian.dilley@f311dafe-5a3c-11de-9a1b-6103e4980ab8>
2010-12-03 17:18:43 +0000

    created ProxyUtil for creating proxied interfaces that use underlying JsonRpcClients

---
+++ jsonrpc4j/src/main/java/com/googlecode/jsonrpc4j/ProxyUtil.java
@@ -0,0 +1,91 @@
+package com.googlecode.jsonrpc4j;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProxyUtil {
+
+	/**
+	 * Creates a {@link Proxy} of the given {@link proxyInterface}
+	 * that uses the given {@link JsonRpcClient}.
+	 * @param <T> the proxy type
+	 * @param classLoader the {@link ClassLoader}
+	 * @param proxyInterface the interface to proxy
+	 * @param client the {@link JsonRpcClient}
+	 * @param ips the {@link InputStream}
+	 * @param ops the {@link OutputStream}
+	 * @return the proxied interface
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T createProxy(
+		ClassLoader classLoader,
+		Class<T> proxyInterface,
+		final JsonRpcClient client,
+		final InputStream ips,
+		final OutputStream ops) {
+
+		// create and return the proxy
+		return (T)Proxy.newProxyInstance(
+			ClassLoader.getSystemClassLoader(),
+			new Class<?>[] { proxyInterface },
+			new InvocationHandler() {
+				public Object invoke(Object proxy, Method method, Object[] args)
+					throws Throwable {
+					return client.invoke(
+						method.getName(), args, method.getGenericReturnType(), ops, ips);
+				}
+			});
+	}
+
+	/**
+	 * Creates a {@link Proxy} of the given {@link proxyInterface}
+	 * that uses the given {@link JsonRpcHttpClient}.
+	 * @param <T> the proxy type
+	 * @param classLoader the {@link ClassLoader}
+	 * @param proxyInterface the interface to proxy
+	 * @param client the {@link JsonRpcHttpClient}
+	 * @param extraHeaders extra HTTP headers to be added to each response
+	 * @return the proxied interface
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T createProxy(
+		ClassLoader classLoader,
+		Class<T> proxyInterface,
+		final JsonRpcHttpClient client,
+		final Map<String, String> extraHeaders) {
+
+		// create and return the proxy
+		return (T)Proxy.newProxyInstance(
+			classLoader,
+			new Class<?>[] { proxyInterface },
+			new InvocationHandler() {
+				public Object invoke(Object proxy, Method method, Object[] args)
+					throws Throwable {
+					return client.invoke(
+						method.getName(), args, method.getGenericReturnType(), extraHeaders);
+				}
+			});
+	}
+
+	/**
+	 * Creates a {@link Proxy} of the given {@link proxyInterface}
+	 * that uses the given {@link JsonRpcHttpClient}.
+	 * @param <T> the proxy type
+	 * @param classLoader the {@link ClassLoader}
+	 * @param proxyInterface the interface to proxy
+	 * @param client the {@link JsonRpcHttpClient}
+	 * @return the proxied interface
+	 */
+	public static <T> T createProxy(
+		ClassLoader classLoader,
+		Class<T> proxyInterface,
+		final JsonRpcHttpClient client) {
+		return createProxy(classLoader, proxyInterface, client, new HashMap<String, String>());
+	}
+
+}
